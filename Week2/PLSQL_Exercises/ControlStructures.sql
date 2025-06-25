1e70312dbc8a93352e90f0b53d335b0908624318
2200031021 <2200031021cseh@gmail.com>
2025-06-25 16:35:09 +0530

    .

--- Week2/PLSQL_Exercises/ControlStructures.sql
+++
@@ -1,9 +0,0 @@
--- MySQL version of Control Structure using IF...ELSE
-SET @marks = 75;
-
--- Use CASE in SELECT for similar logic
-SELECT 
-    CASE 
-        WHEN @marks >= 50 THEN 'Passed'
-        ELSE 'Failed'
-    END AS result;
